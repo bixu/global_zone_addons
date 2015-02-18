21c17cdafa5a133b0f501bf8372af74ce2c1156a
Blake Irvin <bixv@icloud.com>
2015-02-18 13:23:55 +0100

    typo

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 curl -skL https://github.com/bixu/global_zone_addons/tarball/master | gtar xvz
 
-cd $(ls -lrtd global_zone_addons* | tail -n1 | awk '{print $9}')
+cd $(ls -lrtd bixu-global_zone_addons* | tail -n1 | awk '{print $9}')
 
 for script in $(ls ./addons.d); do
 	./addons.d/$script
