574eec484a60a8b036f44cdfe24503e890681abc
Blake Irvin <bixv@icloud.com>
2015-02-18 13:19:58 +0100

    smarter cd

--- install.sh
+++ install.sh
@@ -2,9 +2,7 @@
 
 curl -skL https://github.com/bixu/global_zone_addons/tarball/master | gtar xvz
 
-mv bixu-global_zone_addons-* global_zone_addons
-
-cd global_zone_addons
+cd $(ls -lrtd global_zone_addons* | tail -n1 | awk '{print $9}')
 
 for script in $(ls addons.d); do
 	$script
