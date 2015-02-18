e984e5d48cbbd54d75b9c0dd59ecd9a953585e03
Blake Irvin <bixv@icloud.com>
2015-02-18 13:21:43 +0100

    smarter recursive script run

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ curl -skL https://github.com/bixu/global_zone_addons/tarball/master | gtar xvz
 
 cd $(ls -lrtd global_zone_addons* | tail -n1 | awk '{print $9}')
 
-for script in $(ls addons.d); do
-	$script
+for script in $(ls ./addons.d); do
+	./addons.d/$script
 done
