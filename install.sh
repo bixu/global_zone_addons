302ae1d08a0d2ab9843f0d3267b7083e6b181401
Blake Irvin <bixv@icloud.com>
2015-02-18 13:48:58 +0100

    make tar quieter

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -skL https://github.com/bixu/global_zone_addons/tarball/master | gtar xvz
+curl -skL https://github.com/bixu/global_zone_addons/tarball/master | gtar xz
 
 cd $(ls -lrtd bixu-global_zone_addons* | tail -n1 | awk '{print $9}')
 
